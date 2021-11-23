c1b24ab994cd4e52f42ce0136968a00e357f8f99
val.istar.guo <val.istar.guo@gmail.com>
2021-11-23 15:36:47 +0800

    refactor: rename app-module.ts to app.module.ts

--- templates/src/index.ts
+++ templates/src/index.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core'
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
-import { AppModule } from './app-module'
+import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
 import * as SELF from '@/config/self'
 import '@/config'
